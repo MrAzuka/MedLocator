0c327de37ec35379e6ae0d7dcbe83fac75842ce8
MrAzuka <johntwice8@gmail.com>
2025-06-12 00:54:45 +0100

    Entity setup

---
+++ src/main/java/com/mrazuka/medlocator/Model/DrugModel.java
@@ -0,0 +1,130 @@
+package com.mrazuka.medlocator.Model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class DrugModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String drugName;
+    private String chemicalName;
+    private String description;
+
+//    int and double are primitive types and cannot be null.
+//    In the context of quantity and price,
+//    it's generally expected that a drug will always have a quantity and a price.
+//    If these values were allowed to be null,
+//    it could lead to unexpected behavior or invalid data.
+//    Using primitives enforces that these fields always have a value (defaulting to 0 if not explicitly set).
+    private double price;
+    private int quantity;
+
+    /**
+     * Many-to-One relationship with the Store entity.
+     * Multiple drugs can belong to one store.
+     * The @JoinColumn annotation specifies the foreign key column (store_id)
+     * in the Drug table that references the primary key of the Store table.
+     */
+    @ManyToOne
+    @JoinColumn(name = "store_id", nullable = false) // Ensures that a drug must always be associated with a store
+    private StoreModel store; // The store to which this drug belongs
+
+    public DrugModel() {
+    }
+
+    public DrugModel(Integer id, String drugName,
+                     String chemicalName, String description,
+                     double price, int quantity, StoreModel store) {
+        this.id = id;
+        this.drugName = drugName;
+        this.chemicalName = chemicalName;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+        this.store = store;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDrugName() {
+        return drugName;
+    }
+
+    public void setDrugName(String drugName) {
+        this.drugName = drugName;
+    }
+
+    public String getChemicalName() {
+        return chemicalName;
+    }
+
+    public void setChemicalName(String chemicalName) {
+        this.chemicalName = chemicalName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public StoreModel getStore() {
+        return store;
+    }
+
+    public void setStore(StoreModel store) {
+        this.store = store;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        DrugModel drugModel = (DrugModel) o;
+        return Double.compare(price, drugModel.price) == 0 && quantity == drugModel.quantity && Objects.equals(id, drugModel.id) && Objects.equals(drugName, drugModel.drugName) && Objects.equals(chemicalName, drugModel.chemicalName) && Objects.equals(description, drugModel.description) && Objects.equals(store, drugModel.store);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, drugName, chemicalName, description, price, quantity, store);
+    }
+
+    @Override
+    public String toString() {
+        return "DrugModel{" +
+                "id=" + id +
+                ", drugName='" + drugName + '\'' +
+                ", chemicalName='" + chemicalName + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                ", store=" +  (store != null ? store.getId() : "null") +
+                '}';
+    }
+}
---
+++ src/main/java/com/mrazuka/medlocator/Model/StoreModel.java
@@ -0,0 +1,146 @@
+package com.mrazuka.medlocator.Model;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+public class StoreModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String storeName;
+    private String storeAddress;
+    private String ownerName;
+    private String ownerEmail;
+    private String ownerPassword;
+    private String contactPhone;
+    private String contactEmail;
+
+    // Optionally, if you want to access drugs from a store (one-to-many relationship)
+    @OneToMany(mappedBy = "store") // 'store' is the field name in the Drug entity
+    private Set<DrugModel> drugs;
+
+
+    public StoreModel() {
+
+    }
+
+    public StoreModel(UUID id, String storeName,
+                      String storeAddress, String ownerName,
+                      String ownerEmail, String ownerPassword,
+                      String contactPhone, String contactEmail,
+                      Set<DrugModel> drugs) {
+        this.id = id;
+        this.storeName = storeName;
+        this.storeAddress = storeAddress;
+        this.ownerName = ownerName;
+        this.ownerEmail = ownerEmail;
+        this.ownerPassword = ownerPassword;
+        this.contactPhone = contactPhone;
+        this.contactEmail = contactEmail;
+        this.drugs = drugs;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public String getStoreAddress() {
+        return storeAddress;
+    }
+
+    public void setStoreAddress(String storeAddress) {
+        this.storeAddress = storeAddress;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getOwnerEmail() {
+        return ownerEmail;
+    }
+
+    public void setOwnerEmail(String ownerEmail) {
+        this.ownerEmail = ownerEmail;
+    }
+
+    public String getOwnerPassword() {
+        return ownerPassword;
+    }
+
+    public void setOwnerPassword(String ownerPassword) {
+        this.ownerPassword = ownerPassword;
+    }
+
+    public String getContactPhone() {
+        return contactPhone;
+    }
+
+    public void setContactPhone(String contactPhone) {
+        this.contactPhone = contactPhone;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public Set<DrugModel> getDrugs() {
+        return drugs;
+    }
+
+    public void setDrugs(Set<DrugModel> drugs) {
+        this.drugs = drugs;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        StoreModel that = (StoreModel) o;
+        return Objects.equals(id, that.id) && Objects.equals(storeName, that.storeName) && Objects.equals(storeAddress, that.storeAddress) && Objects.equals(ownerName, that.ownerName) && Objects.equals(ownerEmail, that.ownerEmail) && Objects.equals(ownerPassword, that.ownerPassword) && Objects.equals(contactPhone, that.contactPhone) && Objects.equals(contactEmail, that.contactEmail) && Objects.equals(drugs, that.drugs);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, storeName, storeAddress, ownerName, ownerEmail, ownerPassword, contactPhone, contactEmail, drugs);
+    }
+
+    @Override
+    public String toString() {
+        return "StoreModel{" +
+                "id=" + id +
+                ", storeName='" + storeName + '\'' +
+                ", storeAddress='" + storeAddress + '\'' +
+                ", ownerName='" + ownerName + '\'' +
+                ", ownerEmail='" + ownerEmail + '\'' +
+                ", contactPhone='" + contactPhone + '\'' +
+                ", contactEmail='" + contactEmail + '\'' +
+                ", drugs=" + drugs +
+                '}';
+    }
+}
