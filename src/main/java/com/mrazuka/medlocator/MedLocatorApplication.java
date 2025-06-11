f7be931752244e2cf7f6ba1175c328295dc0210c
MrAzuka <johntwice8@gmail.com>
2025-06-11 22:20:50 +0100

    Initialization and DB Setup

---
+++ src/main/java/com/mrazuka/medlocator/MedLocatorApplication.java
@@ -0,0 +1,13 @@
+package com.mrazuka.medlocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedLocatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedLocatorApplication.class, args);
+    }
+
+}
